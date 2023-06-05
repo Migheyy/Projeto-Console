2653ef36926241c2e74dd993a26fb53782f11873
Migheyy <119352011+Migheyy@users.noreply.github.com>
2023-06-05 15:16:20 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+var nome = "Miguel ";
+var sobreNome = "Moraes";
+var join = nome.concat(sobreNome);
+console.log(join);
+
+var original = "Eu sou Miguel Moraes";
+var replacement = original.replace ("Moraes","Sousa");
+console.log(replacement);
+
+var change = "MIGUEL"
+var changed = change.toLowerCase(change)
+console.log(changed);
+
+var a = 8;
+var b = 4;
+var add = a + b;
+console.log(add);
+
+var sub = a - b;
+console.log(sub);
+
+var multiple = a * b;
+console.log(multiple);
+
+var divide = a / b;
+console.log(divide);
+
+var students = ["Lucca","Clara","Tiago","Rafael"];
+console.log(students);
